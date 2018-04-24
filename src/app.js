8b13577d61c415ef784c542d68cec5641089c311
samirasharma <samira.sharma@outlook.com>
2018-04-24 14:48:24 +0545

    header added

---
+++ src/app.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+// 
+// class App extends Component{
+// 	render() {
+// 		return (
+// 				<View>
+// 					<Text>An app</Text>
+// 				</View>
+// 			);
+// 	}
+// }
---
+++ src/components/header.js
@@ -0,0 +1,10 @@
+//import libraries for making component
+import React from 'react';
+import { Text } from 'react-native';
+
+//Make component
+const Header = () => {
+	return <Text>Login</Text>;
+};
+
+export default Header;
