087acaa034bb0fe0e99c0245cbf1bfd10a346ce2
samirasharma <samira.sharma@outlook.com>
2018-04-26 12:42:47 +0545

    card component added and passed props

--- src/app.js
+++ src/app.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
-// 
-// class App extends Component{
-// 	render() {
-// 		return (
-// 				<View>
-// 					<Text>An app</Text>
-// 				</View>
-// 			);
-// 	}
-// }
+import { View, Text } from 'react-native';
+
+class App extends Component {
+	render() {
+		return (
+			<View>
+				<Text>An app </Text>
+			</View>
+		);
+	}
+}
+
+export default App;
