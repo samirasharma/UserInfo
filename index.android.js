d7a910d09604219c5a2e791f3f2ef1177d9d2da4
samirasharma <samira.sharma@outlook.com>
2018-04-27 09:09:56 +0545

    components added and exported from index file

---
+++ index.android.js
@@ -0,0 +1,14 @@
+// import React from 'react';
+// import { AppRegistry} from 'react-native';
+// import Header from './src/components/header';
+
+// const App = () => (
+// 		<Header headerText={'User Login'}/>
+// 	);
+
+// AppRegistry.registerComponent('UserInfo',() => App);
+
+import { AppRegistry } from 'react-native';
+import App from './src/App';
+
+AppRegistry.registerComponent('UserInfo', () => App);
---
+++ src/components/common/Button.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Text, TouchableOpacity } from 'react-native';
+
+const Button = () =>{
+	const { buttonStyle } = styles;
+	return (
+		<TouchableOpacity style = {buttonStyle}>
+			<Text style = {textStyle}> 
+				Click Me !!!
+			</Text>
+		</TouchableOpacity>
+	);
+};
+
+const styles = {
+	textStyle: {
+		alignSelf: 'center', 
+		color: '#007aff', 
+		fontSize: 16, 
+		fontWeight: '600',
+		paddingTop: 10, 
+		paddingBottom: 10
+	}
+	buttonStyle: {
+		flex: 1, 
+		alignSelf: 'stretch',
+		backgroundColor: '#fff', 
+		borderWidth: 1, 
+		borderColor: '#007aff',
+		marginLeft: 5, 
+		marginRight: 5
+	}
+}
+
+export { Button };
