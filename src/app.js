c4503fd29d244d009f10e3105b96a0e83327d88f
samirasharma <samira.sharma@outlook.com>
2018-04-30 12:23:54 +0545

    cardsection added for each section of LoginForm

--- src/app.js
+++ src/app.js
@@ -1,13 +1,25 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
+import LoginForm from './components/LoginForm'
 
 class App extends Component {
+	componentWillMount(){
+		firebase.initializeApp({
+    apiKey: "AIzaSyDtKFqL6I0vZNM0kVUA8zO0LV_P1Y7Tc5g",
+    authDomain: "authentication123-78bae.firebaseapp.com",
+    databaseURL: "https://authentication123-78bae.firebaseio.com",
+    projectId: "authentication123-78bae",
+    storageBucket: "authentication123-78bae.appspot.com",
+    messagingSenderId: "511407383730"
+  });
+	}
 	render() {
 		return (
 			<View>
 				<Header headerText="Authentication" />
-				<Text>An app </Text>
+				<LoginForm />
 			</View>
 		);
 	}
