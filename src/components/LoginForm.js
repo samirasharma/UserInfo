5a823104aade6a42887cf117bdc0bfd4ad873401
samirasharma <samira.sharma@outlook.com>
2018-05-02 17:37:50 +0545

    login form designed and command added for onpress of login button

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -1,22 +1,62 @@
 import React, { Component } from 'react';
+import { Text} from 'react'
 import { View } from 'react-native';
+import firebase from 'firebase';
 import { Button, Card, CardSection } from './common';
 
 class LoginForm extends Component {
+	state = { email: '', password: '', error: ''};
+	onButtonPress(){
+		const { email, password } = this.state;
+		firebase.auth().signInWithEmailAndPassword(email, password);
+			.catch(() => {
+			firebase.auth().createUserWitheEmailAndPassword(email, passowrd)
+				.catch(() => {
+						this.setState({error: 'Authentication Failed'});
+				});
+			});
+
+	}
 	render() {
 		return (
 			<Card>
-				<CardSection />
-				<CardSection />
+				<CardSection >
+					<Input 
+						placeholder="user@gmail.com"
+						label="Email"
+						value={this.state.email}
+						onChangeText={email => this.setState({ email})}
+			 />
+				</CardSection>
 
 				<CardSection>
-					<Button>
+					<Input
+						secureTextEntry
+						placeholder="password"
+						label="Password"
+						value={this.state.password}
+						onChangeText={password =>this.setState({ password })}
+				</CardSection>
+				<Text style ={styles.errorTextStyle}>
+					{this.state.error}
+				</Text>
+
+				<CardSection>
+					<Button onPress={this.onButtonPress.bind(this)}>
 						Log In
 					</Button>
 				</CardSection>
 			</Card>
+			
 		);
 	}
 }
+ const styles = {
+ 	errorTextStyle: {
+ 		fontSize: 20, 
+ 		alignSetlf: 'center', 
+ 		color: 'red'
+ 	}
+ }
 
 export default LoginForm;
